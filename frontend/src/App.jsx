2f3784f84880ae962de1e0e67bf70da3eca8675c
Akshat <akshatjaiswal2518@gmail.com>
2025-07-31 23:41:56 +0530

    setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center">Welcome to Wellness App</h1>
+      <p className="text-center">Your journey to better health starts here.</p>
+    </>
+  );
+}
+
+export default App;
